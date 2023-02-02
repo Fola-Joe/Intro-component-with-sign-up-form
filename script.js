3c8ced30a0019a2989b203c0edb2859b3a4cce18
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-02-02 12:16:21 +0100

    Update script.js
    Tying to get the error msg active onclick with btn

--- script.js
+++ script.js
@@ -1,19 +1,67 @@
-const fname = document.querySelector('#first-name');
-const lname = document.querySelector('#last-name');
-const email = document.querySelector('#email');
-const password = document.querySelector('password');
+// const fname = document.querySelector('#first-name');
+// const lname = document.querySelector('#last-name');
+// const email = document.querySelector('#email');
+const input = document.querySelectorAll('.input');
+// const password = document.querySelector('password');
 const btn = document.querySelector('#btn');
+const errorIcon = document.querySelectorAll('.error-icon');
+const errorLine = document.querySelectorAll('.para');
 
-btn.addEventListener ('submit', function(e) {
-    e.preventDefault();
-    const errorIcon = document.querySelector('.error-icon');
-    const errorLine = document.querySelector('.para');
-    let firstName = fname.value;
-    let lastName = lname.value;
-    let eMail = email.value;
-    let passWord = password.value;
-    if (firstName = '') {
-        errorIcon.style.display = 'flex';
-        errorLine.style.display = 'block';
+// function validateEmail (email) {
+//     const err = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+//     return err.test(String(email).toLowerCase());
+// }
+
+function validity() {
+    const fname = document.querySelector('#first-name');
+    const lname = document.querySelector('#last-name');
+    const email = document.querySelector('#email');
+    const password = document.querySelector('password');
+
+    const fnameValue = fname.value.trim();
+    const lnameValue = lname.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (fnameValue === '') {
+        errorIcon[0].style.display = 'flex';
+        errorLine[0].style.display = 'block';
+    } else {
+        errorIcon[0].style.display = 'none';
+        errorLine[0].style.display = 'none';
     }
+}
+
+btn.addEventListener('click', (e) => {
+    // e.preventDefault();
+    validity();
 })
+
+// input[0].addEventListener('input', (e) => {
+//     const value = e.target.value;
+//     if (value.length <= 0) {
+//         errorIcon.style.display = 'flex';
+//         errorLine.style.display = 'block';
+//     } else {
+//         errorIcon.style.display = 'none';
+//         errorLine.style.display = 'none';
+//     }
+// });
+
+
+
+
+
+
+
+// btn.addEventListener ('submit', function(e) {
+//     e.preventDefault();
+//     let firstName = fname.value;
+//     let lastName = lname.value;
+//     let eMail = email.value;
+//     let passWord = password.value;
+//     if (firstName = '') {
+//         errorIcon.style.display = 'flex';
+//         errorLine.style.display = 'block';
+//     }
+// })
